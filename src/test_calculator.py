ba5b557987ba49ebd1ddd29493e6ebfdc919aa9b
David <davidglez_cruz@hotmail.com>
2025-11-28 22:54:00 -0600

    test 2

--- src/test_calculator.py
+++ src/test_calculator.py
@@ -41,8 +41,8 @@ class TestCalculatorOperations(unittest.TestCase):
 
     def test_addition(self):
         """Prueba que la suma de enteros sea correcta."""
-        result = self.perform_operation('5', '+', '3')
-        self.assertEqual(result, '9')
+        result = self.perform_operation('5', '+', '4')
+        self.assertEqual(result, '8')
 
         # Prueba con decimales
         result_float = self.perform_operation('10.5', '+', '2.2')
